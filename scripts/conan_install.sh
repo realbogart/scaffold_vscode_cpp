f4b1ee28192e2a762db3630fcc2f1df4f7c8d378
Johan Yngman <johan.yngman@gmail.com>
2023-03-16 22:07:02 +0100

    Conan 2.0 fixes

--- scripts/conan_install.sh
+++ scripts/conan_install.sh
@@ -5,5 +5,5 @@ source ${SCRIPT_DIR}/env.sh
 
 conan config install ${SOURCE_DIR}/.conan_config
 
-conan install . -pr linux-clang-debug --build=missing -if ${CONAN_OUTPUT_DIR}
-conan install . -pr linux-clang-release --build=missing -if ${CONAN_OUTPUT_DIR}
+conan install . -pr:b linux-clang-debug -pr:h linux-clang-debug --build=missing -of ${CONAN_OUTPUT_DIR}
+conan install . -pr:b linux-clang-release -pr:h linux-clang-release --build=missing -of ${CONAN_OUTPUT_DIR}
